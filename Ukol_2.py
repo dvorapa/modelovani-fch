c52b36811d9dabe639848d3198bcf61d4f7bc7b0
Pavel Dvořák <dvorapa@users.noreply.github.com>
2018-03-16 11:56:47 +0100

    Doplnění Kaye a Joffeho do úkolu 2

--- Ukol_2.py
+++ Ukol_2.py
@@ -1,6 +1,8 @@
 ### Skript pro výpočet hustoty dvojice kapalin kyselina octová - 1,2-dichloropropan
 from sympy import *
 Vm = Symbol('Vm')
+Tk = Symbol('Tk')
+pk = Symbol('pk')
 R = 8.314
 
 ## Nadefinované podmínky
@@ -18,41 +20,116 @@ pk_dcp = 46.5 * 10**5 # Pa
 Mw_dcp = 112.986 # g/mol
 
 ## Redlich-Kwong
-a = 0.42748*(R**2)*(Tk_oct**2.5)/pk_oct
-b = 0.08664*R*Tk_oct/pk_oct
-rovnice_ = R*T/(Vm-b) - a/(sqrt(T)*Vm*(Vm+b)) - p
-koreny_oct = solve(rovnice_, Vm)
+a = (0.42748*(R**2)*(Tk**2.5))/pk
+b = (0.08664*R*Tk)/pk
+rovnice_ = ((R*T)/(Vm-b)) - (a/(sqrt(T)*Vm*(Vm+b))) - p
+
+koreny_oct = solve(rovnice_.subs([(Tk, Tk_oct), (pk, pk_oct)]), Vm)
 koreny_oct_re = []
 for k in koreny_oct:
-    koreny_oct_re.append(re(k))
+    if im(k) < 10**21:
+        koreny_oct_re.append(re(k))
 Vm_oct = min(koreny_oct_re)
+ro_oct_ = 5.84 # mol/l (NIST, 2018)
+ro_oct = 1/(Vm_oct*(10**3))
 
-a = 0.42748*(R**2)*(Tk_dcp**2.5)/pk_dcp
-b = 0.08664*R*Tk_dcp/pk_dcp
-rovnice_ = R*T/(Vm-b) - a/(sqrt(T)*Vm*(Vm+b)) - p
-koreny_dcp = solve(rovnice_, Vm)
+koreny_dcp = solve(rovnice_.subs([(Tk, Tk_dcp), (pk, pk_dcp)]), Vm)
 koreny_dcp_re = []
 for k in koreny_dcp:
-    koreny_dcp_re.append(re(k))
+    if im(k) < 10**21:
+        koreny_dcp_re.append(re(k))
 Vm_dcp = min(koreny_dcp_re)
+ro_dcp_ = 3.452 # mol/l (NIST, 2018)
+ro_dcp = 1/(Vm_dcp*(10**3))
 
 # Amagat
 Vm_rk = (0.25*Vm_oct + 0.75*Vm_dcp)
-Mw_rk = (0.25*Mw_oct + 0.75*Mw_dcp)
-ro_rk_a = Mw_rk/(Vm_rk*(10**6))
+ro_rk_a = 1/(Vm_rk*(10**3))
+
+# Kay
+Tk_ = 0.25*Tk_oct + 0.75*Tk_dcp
+pk_ = 0.25*pk_oct + 0.75*pk_dcp
+
+koreny_ = solve(rovnice_.subs([(Tk, Tk_), (pk, pk_)]), Vm)
+koreny_re_ = []
+for k in koreny_:
+    if im(k) < 10**21:
+        koreny_re_.append(re(k))
+Vm_ = min(koreny_re_)
+ro_rk_k = 1/(Vm_*(10**3))
+
+# Joffe
+K1 = (((0.25*Tk_oct)/sqrt(pk_oct)) + ((0.75*Tk_dcp)/sqrt(pk_dcp)))
+K2 = 0
+seznam_x = (0.25, 0.75)
+seznam_Tk = (Tk_oct, Tk_dcp)
+seznam_pk = (pk_oct, pk_dcp)
+for i in range(2):
+    for j in range(2):
+        K2 += seznam_x[i]*seznam_x[j]*((((seznam_Tk[i]/seznam_pk[i])**(1/3))+((seznam_Tk[j]/seznam_pk[j])**(1/3)))**3)
+K2 *= 0.125
+
+Tk_ = (K1**2)/K2
+pk_ = (K1/K2)**2
+
+koreny_ = solve(rovnice_.subs([(Tk, Tk_), (pk, pk_)]), Vm)
+koreny_re_ = []
+for k in koreny_:
+    if im(k) < 10**21:
+        koreny_re_.append(re(k))
+Vm_ = min(koreny_re_)
+ro_rk_j = 1/(Vm_*(10**3))
 
 ## Rackett
-Vk_oct = R*Tk_oct/pk_oct
-Vm_oct = Vk_oct*((pk_oct*Vk_oct/(R*Tk_oct))**(((1-T)/Tk_oct)**(2/7)))
+Vk = R*Tk/pk
+rovnice_ = Vk*(((pk*Vk)/(R*Tk))**(((1-T)/Tk)**(2/7)))
 
-Vk_dcp = R*Tk_dcp/pk_dcp
-Vm_dcp = Vk_dcp*((pk_dcp*Vk_dcp/(R*Tk_dcp))**(((1-T)/Tk_dcp)**(2/7)))
+Vm_oct = N(rovnice_.subs([(Tk, Tk_oct), (pk, pk_oct)]))
+if im(Vm_oct) < 10**21:
+    Vm_oct = re(Vm_oct)
+
+Vm_dcp = N(rovnice_.subs([(Tk, Tk_dcp), (pk, pk_dcp)]))
+if im(Vm_dcp) < 10**21:
+    Vm_dcp = re(Vm_dcp)
 
 # Amagat
 Vm_r = (0.25*Vm_oct + 0.75*Vm_dcp)
-Mw_r = (0.25*Mw_oct + 0.75*Mw_dcp)
-ro_r_a = Mw_r/(Vm_r*(10**6))
+ro_r_a = 1/(Vm_r*(10**3))
+
+# Kay
+Tk_ = 0.25*Tk_oct + 0.75*Tk_dcp
+pk_ = 0.25*pk_oct + 0.75*pk_dcp
+
+Vm_ = N(rovnice_.subs([(Tk, Tk_), (pk, pk_)]))
+if im(Vm_) < 10**21:
+    Vm_ = re(Vm_)
+ro_r_k = 1/(Vm_*(10**3))
+
+# Joffe
+K1 = (((0.25*Tk_oct)/sqrt(pk_oct)) + ((0.75*Tk_dcp)/sqrt(pk_dcp)))
+K2 = 0
+seznam_x = (0.25, 0.75)
+seznam_Tk = (Tk_oct, Tk_dcp)
+seznam_pk = (pk_oct, pk_dcp)
+for i in range(2):
+    for j in range(2):
+        K2 += seznam_x[i]*seznam_x[j]*((((seznam_Tk[i]/seznam_pk[i])**(1/3))+((seznam_Tk[j]/seznam_pk[j])**(1/3)))**3)
+K2 *= 0.125
+
+Tk_ = (K1**2)/K2
+pk_ = (K1/K2)**2
+
+Vm_ = N(rovnice_.subs([(Tk, Tk_), (pk, pk_)]))
+if im(Vm_) < 10**21:
+    Vm_ = re(Vm_)
+ro_r_j = 1/(Vm_*(10**3))
 
 # Výsledné výsledky
-print('ro_rk_a = ' + str(re(ro_rk_a)) + ' g/cm^3\n')
-print('ro_r_a = ' + str(re(ro_r_a)) + ' g/cm^3\n')
+print('ro_oct = ' + str(ro_oct) + ' mol/l\n')
+print('ro_dcp = ' + str(ro_dcp) + ' mol/l\n')
+print('ro_smes_rk_a = ' + str(ro_rk_a) + ' mol/l\n')
+print('ro_smes_rk_k = ' + str(ro_rk_k) + ' mol/l\n')
+print('ro_smes_rk_j = ' + str(ro_rk_j) + ' mol/l\n')
+print('ro_smes_r_a = ' + str(ro_r_a) + ' mol/l\n')
+print('ro_smes_r_k = ' + str(ro_r_k) + ' mol/l\n')
+print('ro_smes_r_j = ' + str(ro_r_j) + ' mol/l\n')
