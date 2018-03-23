f782b870b6b2f9bc8367eefa5961222652fceb22
Pavel Dvořák <dvorapa@users.noreply.github.com>
2018-03-23 09:47:24 +0100

    Dokončení úkolu 3

--- Ukol_3.py
+++ Ukol_3.py
@@ -9,7 +9,6 @@ R = 8.314
 
 ## Nadefinované podmínky
 Tp = 273.15 + 25 # K
-pp = 101325 # Pa
 
 # Kritické veličiny pro chlorobenzen (Young, 1910; NIST, 2018)
 Tk_chb = 632.35 # K
@@ -20,10 +19,18 @@ A = 4.11083
 B = 1435.675
 C = -55.124
 
+## Antoine
+def p(T):
+    return (10**(A-(B/(T+C)))) * 10**5
+numdiff_p = derivative(p, Tp, dx=1e-6)
+
+p = (10**(A-(B/(T+C)))) * 10**5
+analdiff_p = diff(p, T).subs(T, Tp)
+
 ## Redlich-Kwong
 a = (0.42748*(R**2)*(Tk**2.5))/pk
 b = (0.08664*R*Tk)/pk
-rovnice_ = ((R*Tp)/(Vm-b)) - (a/(sqrt(Tp)*Vm*(Vm+b))) - pp
+rovnice_ = ((R*Tp)/(Vm-b)) - (a/(sqrt(Tp)*Vm*(Vm+b))) - p.subs(T, Tp)
 
 koreny_chb = solve(rovnice_.subs([(Tk, Tk_chb), (pk, pk_chb)]), Vm)
 koreny_chb_re = []
@@ -36,14 +43,6 @@ Vm_l = min(koreny_chb_re)
 assert Vm_g != Vm_l
 del_Vm = Vm_g - Vm_l
 
-## Antoine
-p = (10**(A-(B/(T+C)))) * 10**5
-analdiff_p = diff(p, T).subs(T, Tp)
-
-def p(T):
-    return (10**(A-(B/(T+C)))) * 10**5
-numdiff_p = derivative(p, Tp, dx=1e-6)
-
 ## Clapeyron
 del_H_num = numdiff_p * Tp * del_Vm
 del_H_anal = analdiff_p * Tp * del_Vm
@@ -53,7 +52,7 @@ del_H_anal = N(del_H_anal/1000)
 # Výsledné výsledky
 print('del_H_vyp_num = ' + str(del_H_num) + ' kJ/mol\n')
 print('del_H_vyp_anal = ' + str(del_H_anal) + ' kJ/mol\n')
-print('del_H_vyp_tab = 48.1 kJ/mol (Liu and Dickhut, 1994; NIST 2018)\n')
+print('del_H_vyp_tab = 41 ± 4 kJ/mol (průměr více exp. hodnot; NIST 2018)\n')
 
 #http://docs.sympy.org/latest/tutorial/calculus.html
 #https://docs.scipy.org/doc/scipy/reference/generated/scipy.misc.derivative.html
