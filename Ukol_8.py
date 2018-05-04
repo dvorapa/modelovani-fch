00c030854b513037ad6ca6b4ccf683da138c7272
dvorapa <dvorapa@seznam.cz>
2018-05-04 19:34:02 +0200

    Vykreslení grafu závislosti v úkolu 8

--- Ukol_8.py
+++ Ukol_8.py
@@ -1,10 +1,11 @@
 ### Skript pro výpočet řádu reakce a rychlostní konstanty
 from sympy import *
 import numpy as np
+import matplotlib.pyplot as plt
 R = 8.314
 
 ## Nadefinované podmínky
-T = 293.15 + 50 # K
+T = 273.15 + 50 # K
 
 # Koncentrace
 t = [0, 40, 70, 220, 320, 404, 642] # s
@@ -23,6 +24,21 @@ while i < len(t):
 x = np.array(ci, dtype=float)
 y = np.array(r, dtype=float)
 vysl = np.polyfit(x, y, 1)
+
+## Tisk grafu
+plt.plot(x, y)
+
+yfit = []
+i=0
+while i < len(x):
+    yfit.append(vysl[0]*x[i] + vysl[1])
+    i += 1
+plt.plot(x, yfit)
+
+plt.xlabel('ln(c)')
+plt.ylabel('ln(r)')
+plt.title('Graf korelace regresní křivky (červeně) a dat (modře)')
+plt.savefig('Ukol_8.png')
 
 ## Výsledný řád reakce a rychlostní konstanta
 print('alpha = ' + str(N(vysl[0])))
