d4113f2db945d8f8bc1c75c3df2460bd7caff402
dvorapa <dvorapa@seznam.cz>
2018-05-05 23:12:03 +0200

    Dokončení úkolu 6

--- Ukol_6.py
+++ Ukol_6.py
@@ -6,24 +6,23 @@ zA, zK = symbols('zA zK', real=True)
 
 ## Nadefinované podmínky
 T = 273.15 + 25 # K
-c = 0.07 # M (mol/l = mol/kg) #########################
-cw = 997/18.016
+c = 0.07 # M (mol/l)
+cw = 997/18.016 # mol/l
 
 ## Tabulkové hodnoty disociačních koeficientů (Wikipedie, 2018)
 pK_H2BO3_ = 9.24
 pK_HBO3_2 = 12.4
 pK_BO3_3 = 13.3
-pK_H2O_ = 14
+pK_H2O = 14
 
 ## Mezivýpočet disociačních konstant K
 K1 = 10**(-pK_H2BO3_)
 K2 = 10**(-pK_HBO3_2)
 K3 = 10**(-pK_BO3_3)
-Kw = 10**(-pK_H2O_)
+Kw = 10**(-pK_H2O)
 
 ## Iterace
-x_old = [1, 2, 3, 4]
-x_old = [x*(10**-9) for x in x_old]
+x_old = [6.32200549982070e-6, 3.95048911992951e-13, 3.10775588446771e-21, 8.68626027795196e-8]
 while True:
     # Bilance
     m1 = c-x1 # H3BO3
@@ -42,7 +41,7 @@ while True:
 
     # Výpočet koeficientů gamma z původních x
     gam = gam.subs([(x1, x_old[0]), (x2, x_old[1]), (x3, x_old[2]), (x4, x_old[3])])
-    print(gam)
+    print('gamma = ' + str(gam))
     gam11 = gam.subs([(zA, 1), (zK, 1)])
     gam21 = gam.subs([(zA, 2), (zK, 1)])
     gam31 = gam.subs([(zA, 3), (zK, 1)])
@@ -52,7 +51,7 @@ while True:
     rovn2 = m5*m3*gam21/m2 - K2
     rovn3 = m5*m4*gam11*gam31/(m3*gam21) - K3
     rovnw = m5*m6*gam11*gam11/m7 - Kw
-    x_new = nsolve([rovn1, rovn2, rovn3, rovnw], [x1, x2, x3, x4], x_old, tol=10**-14)
+    x_new = nsolve([rovn1, rovn2, rovn3, rovnw], [x1, x2, x3, x4], x_old)
 
     # Porovnání původních a nových x
     if x_old == [x for x in x_new]:
